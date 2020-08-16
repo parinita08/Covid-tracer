3aaf9a0d6527607c00aa741518ace9f6e34a0d3d
Parinita Vedantam <vedantampari@gmail.com>
2020-08-16 12:09:12 +0530

    Chart -> done

--- src/api/index.js
+++ src/api/index.js
@@ -24,7 +24,13 @@ export const fetchDailyData = async () => {
     try {
       const { data } = await axios.get(`${url}/daily`);
   
-      return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+      const modifiedData = data.map((dailyData) => ({
+        confirmed: dailyData.confirmed.total,
+        deaths: dailyData.deaths.total,
+        date: dailyData.reportDate,
+      }));
+
+      return modifiedData;
     } catch (error) {
       return error;
     }
